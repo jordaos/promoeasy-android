0b8f09642abcae09b4e13965f90c52e1ac47076f
Jordão <jordao05@hotmail.com>
2017-10-19 01:12:29 -0300

    fix bug

--- app/src/main/java/com/jordao/promoeasy/HomeActivity.java
+++ app/src/main/java/com/jordao/promoeasy/HomeActivity.java
@@ -147,16 +147,6 @@ public class HomeActivity extends AppCompatActivity
 
         if (id == R.id.nav_sorteios) {
             fm.beginTransaction().replace(R.id.app_content, new HomeFragment()).commit();
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         } else if (id == R.id.logout) {
             DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
                 @Override
